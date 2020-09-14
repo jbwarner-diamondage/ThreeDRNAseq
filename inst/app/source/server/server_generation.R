cc6fcb6c97413870fddd15c4b1da855b0b498ac8
Jason B. Warner <Jason.Warner-external@sema4genomics.com>
2020-09-14 15:12:54 -0400

    Testing

--- inst/app/source/server/server_generation.R
+++ inst/app/source/server/server_generation.R
@@ -142,13 +142,14 @@ observe({
 observeEvent(input$mapping_file_button,{
   file_path <- input$mapping_file_button$datapath #
   if (is.null(file_path) | length(file_path)==0)
-    return(NULL)
+    #return(NULL)
     #file_path <- "/home/jwarner/data/Arabidopsis_cold/mapping.csv"
-    #mapping <- read.csv(file = file_path,header = T,fileEncoding="UTF-8-BOM")
-    #colnames(mapping) <- c('TXNAME','GENEID')
-    #rownames(mapping) <- mapping$TXNAME
-    #DDD.data$mapping <- mapping
-    #incProgress(1)
+    file_path <- "/home/jwarner/data/Embryonic_mice/mapping.csv"
+    mapping <- read.csv(file = file_path,header = T,fileEncoding="UTF-8-BOM")
+    colnames(mapping) <- c('TXNAME','GENEID')
+    rownames(mapping) <- mapping$TXNAME
+    DDD.data$mapping <- mapping
+    incProgress(1)
   showmessage('Loading transcript-gene mapping...',
               action = HTML("<i style='font-size:35px;' class='fas fa-coffee'> ... ...</i>"),
               duration = NULL,id = 'message_id')
@@ -196,9 +197,10 @@ observe({
     #return("/home/jwarner/data/Arabidopsis_cold/metatable.csv")
     #file_path <- input$sample_file_button$datapath
     #file_path <- "/home/jwarner/data/Arabidopsis_cold/metatable.csv"
-    #samples0 <- read.csv(file_path,header = T,fileEncoding="UTF-8-BOM")
-    #DDD.data$samples0 <- samples0
-    #incProgress(1)
+    file_path <- "/home/jwarner/data/Embryonic_mice/metatable.csv"
+    samples0 <- read.csv(file_path,header = T,fileEncoding="UTF-8-BOM")
+    DDD.data$samples0 <- samples0
+    incProgress(1)
     return(NULL)
   withProgress(message = 'Loading metadata table...', value = 0, {
     if(!grepl('.csv',file_path))
