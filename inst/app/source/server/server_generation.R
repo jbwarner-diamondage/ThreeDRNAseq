7bb9676fa811042cac8296c03cdd4f6d40fdca64
Jason B. Warner <Jason.Warner-external@sema4genomics.com>
2020-09-15 12:27:28 -0400

    Testing

--- inst/app/source/server/server_generation.R
+++ inst/app/source/server/server_generation.R
@@ -494,7 +494,7 @@ observeEvent(input$samples_update,{
     return(NULL)
   }
 
-  quant_fileNames <- file.path(DDD.data$upload_folder,DDD.data$quant_fileNames[quantfile2check])#DDD.data$quant_fileNames)#
+  quant_fileNames <- file.path(DDD.data$upload_folder,DDD.data$quant_fileNames)#[quantfile2check])
   cat("Checking files ",quant_fileNames)
   if(any(file.exists(quant_fileNames)==F)){
     showmessage('Transcript quantification files are not found. Please double check: (1) whether you have uploaded the quantification compressed folder and
